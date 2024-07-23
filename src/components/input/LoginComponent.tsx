cb78014b0d8410b16d79e5897a0a0f7b278e0ad2
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-07-23 22:53:44 +0545

    corrected

--- src/components/input/LoginComponent.tsx
+++ src/components/input/LoginComponent.tsx
@@ -1,63 +1,59 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
 export default function LoginComponent({
-    placeholder = "Username",
-    type = "text",
+  placeholder = "Username",
+  type = "text",
   // errorMsg = "Invalid Credentials",
 }) {
-const [show, setShow] = useState({ showPassword: false });
+  const [show, setShow] = useState({ showPassword: false });
 
-const toggle = () => {
+  const toggle = () => {
     setShow({ showPassword: !show.showPassword });
-};
-return (
+  };
+  return (
     <div className="h-fit rounded-md grid grid-flow-row my-5 p-1">
-    <label
-        className="text-sm font-semibold text-black"
-        htmlFor="#"
-    ></label>
-    {type == "text" ? (
+      <label className="text-sm font-semibold text-black" htmlFor="#"></label>
+      {type == "text" ? (
         <div className="grid grid-flow-col grid-col-12 w-full border-b-2 mb-1 border-gray-800 mt-1">
-        <div className="col-span-11">
+          <div className="col-span-11">
             <input
-            type="text"
-            placeholder={placeholder}
-            className=" w-full placeholder:text-gray-800 placeholder:font-normal text-black font-normal px-2 p-1 outline-none bg-transparent "
+              type="text"
+              placeholder={placeholder}
+              className=" w-full placeholder:text-gray-800 placeholder:font-normal text-black font-normal px-2 p-1 outline-none bg-transparent "
             />
-        </div>
-        <div className="col-span-1 hover:text-gray-800 text-white overflow-hidden mx-auto w-full">
+          </div>
+          <div className="col-span-1 hover:text-gray-800 text-white overflow-hidden mx-auto w-full">
             <img src="./assets/account.svg" alt="Account Icon" />
+          </div>
         </div>
-        </div>
-    ) : (
+      ) : (
         <div className="grid grid-flow-col grid-col-12 w-full border-b-2 border-gray-800 ">
-        <div className="col-span-11">
+          <div className="col-span-11">
             <input
-            type={show.showPassword ? "text" : "password"}
-            placeholder={placeholder}
-            className=" w-full font-normal h-fit mt-1 placeholder:font-normal placeholder:text-gray-800 text-black px-2 mr-4 outline-none bg-transparent "
+              type={show.showPassword ? "text" : "password"}
+              placeholder={placeholder}
+              className=" w-full font-normal h-fit mt-1 placeholder:font-normal placeholder:text-gray-800 text-black px-2 mr-4 outline-none bg-transparent "
             />
-        </div>
-        <div
+          </div>
+          <div
             onClick={() => toggle()}
             className="col-span-1 hover:text-gray-500 text-black p-1 cursor-pointer overflow-hidden mx-auto w-full"
-        >
+          >
             {show.showPassword == true ? (
-            <img
+              <img
                 className="text-black"
                 src="./assets/eye-off.svg"
                 alt="eyeToggle"
-            />
+              />
             ) : (
-            <img
+              <img
                 className="text-black"
                 src="./assets/eye.svg"
                 alt="eyeToggle"
-            />
+              />
             )}
+          </div>
         </div>
-        </div>
-    )}
+      )}
     </div>
-);
+  );
 }
